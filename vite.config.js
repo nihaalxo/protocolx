e594abf163ff218bb6213cdbab03447f35573b77
Alistair McClymont <alistairmcclymont@gmail.com>
2025-04-25 18:09:16 +0100

    fix: about

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
-import { defineConfig } from 'vite';
-import wasm from 'vite-plugin-wasm';
+import { defineConfig } from "vite";
+import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
   plugins: [wasm()],
-  build: {
-    rollupOptions: {
-      input: {
-        interactive: './index.html',
-        about: './about.html',
-        shaderOne: './shaderOne.html',
-        // ...
-        // List all files you want in your build
-      }
-    }
-  }
 });
