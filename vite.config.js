414fb0289fb0f72e485189281bddf9634957d6bd
Nihal Nazeer <muhammednihal10@gmail.com>
2025-04-25 18:39:41 +0100

    try

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import wasm from 'vite-plugin-wasm';
 
 export default defineConfig({
+  base: './',             // ensure all asset URLs are relative
   plugins: [wasm()],
+  build: {
+    rollupOptions: {
+      input: {
+        // name: absolute path to each html entry
+        main: resolve(__dirname, 'index.html'),
+        interactive: resolve(__dirname, 'interactive/index.html'),
+      }
+    }
+  }
 });
