ebdc1e814b89020359741a83ecb1ceade5f8b952
Alistair McClymont <alistairmcclymont@gmail.com>
2025-04-25 18:12:39 +0100

    move interactive

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
   plugins: [wasm()],
+  build: {
+    rollupOptions: {
+      input: {
+        interactive: "/interactive/index.html",
+        // ...
+        // List all files you want in your build
+      },
+    },
+  },
 });
