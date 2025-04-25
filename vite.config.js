3bfbae8f12d91dc16ca5c0c8aaf830bb18ea9093
Alistair McClymont <alistairmcclymont@gmail.com>
2025-04-25 18:26:35 +0100

    createHtmlPlugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
-import { defineConfig } from 'vite';
-import wasm from 'vite-plugin-wasm';
+import { defineConfig } from "vite";
+import wasm from "vite-plugin-wasm";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
-  plugins: [wasm()],
+  plugins: [
+    wasm(),
+    createHtmlPlugin({
+      minify: true,
+      pages: [
+        {
+          filename: "index.html",
+          template: "index.html",
+        },
+        {
+          filename: "interactive.html",
+          template: "interactive/index.html",
+        }, // Add more pages as needed
+      ],
+    }),
+  ],
 });
